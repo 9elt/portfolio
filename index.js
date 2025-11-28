858c19b17034d2757e3b6a137597fc12d4238d11
9elt <nelt.primary@gmail.com>
2025-11-28 13:49:36 +0100

    feat: use mono font, offuscate number, add data-nosnippet attributes, increase background effect

--- index.js
+++ index.js
@@ -8,36 +8,54 @@ window.addEventListener("mousemove", function (e) {
 });
 var emailElement = document.querySelector(".email");
 var encryptedEmail = emailElement.textContent;
-var email = endecode(encryptedEmail);
+var email = mud(encryptedEmail, 1);
+function showEmail() {
+    window.removeEventListener("click", showEmail);
+    emailElement.removeEventListener("mouseenter", showEmail);
+    emailElement.removeEventListener("click", showEmail);
+    emailElement.href = "mailto:" + email;
+    show(emailElement, encryptedEmail, email);
+}
+var numberElement = document.querySelector(".number");
+var encryptedNumber = numberElement.textContent;
+var number = mud(encryptedNumber, 18);
+function showNumber() {
+    window.removeEventListener("click", showNumber);
+    numberElement.removeEventListener("mouseenter", showNumber);
+    numberElement.removeEventListener("click", showNumber);
+    show(numberElement, encryptedNumber, number);
+}
 if (
     typeof matchMedia !== "undefined"
     && matchMedia("(pointer: coarse)").matches
 ) {
-    window.addEventListener("click", decrypt);
+    window.addEventListener("click", showEmail);
+    window.addEventListener("click", showNumber);
 }
 else {
-    emailElement.addEventListener("mouseenter", decrypt);
-    emailElement.addEventListener("click", decrypt);
+    emailElement.addEventListener("mouseenter", showEmail);
+    numberElement.addEventListener("mouseenter", showNumber);
+    emailElement.addEventListener("click", showEmail);
+    numberElement.addEventListener("click", showNumber);
 }
-function decrypt() {
-    emailElement.removeEventListener("mouseenter", decrypt);
-    emailElement.removeEventListener("click", decrypt);
-    emailElement.href = "mailto:" + email;
+function show(element, from, to) {
+    var ratio = 48 / to.length;
+    var interval = to.length * ratio;
+    var accel = 0.75 * ratio;
     var i = 0;
-    function loop() {
-        emailElement.textContent = email.slice(0, i) + encryptedEmail.slice(i);
-        if (i < email.length) {
-            setTimeout(loop, email.length * 2 - i * 1.5);
+    (function loop() {
+        element.textContent = to.slice(0, i) + from.slice(i);
+        if (i < to.length) {
+            setTimeout(loop, interval - i * accel);
             i++;
         }
-    }
-    loop();
+    })();
 }
-function endecode(str) {
+function mud(str, seed) {
     var result = "";
     var i = str.length;
     while (i--) {
-        result += String.fromCharCode(str.charCodeAt(i) ^ 1);
+        result += String.fromCharCode(str.charCodeAt(i) ^ seed);
     }
     return result;
 }
